1d8a9b5c0aeb13339808f6c78215034724ff0472
Sergey Kirpik <sergeykirpik@gmail.com>
2019-08-10 17:16:31 +0300

    Splitting markup into modules. Ending.

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,6 +1,6 @@
 export const getFilterMarkup = () => {
   return `
-    <form class="trip-filters  trip-filters--hidden" action="#" method="get">
+    <form class="trip-filters" action="#" method="get">
       <div class="trip-filters__filter">
         <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
         <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import {getTripInfoMarkup} from './components/trip-info.js';
+import {getMenuMarkup} from './components/menu';
+import {getFilterMarkup} from './components/filter';
+import {getTripSortMarkup} from './components/trip-sort';
+import {getTripDaysMarkup} from './components/day-list';
+import {getTripEventMarkup} from './components/trip-event';
+import {getEventEditFormMarkup} from './components/event-edit-form';
+
 
 const render = (container, markup, place) => {
   container.insertAdjacentHTML(place, markup);
@@ -7,3 +14,21 @@ const render = (container, markup, place) => {
 const tripMain = document.querySelector(`.trip-main`);
 const tripInfoContainer = tripMain.querySelector(`.trip-info`);
 render(tripInfoContainer, getTripInfoMarkup(), `afterBegin`);
+
+const menuInsertPoint = tripMain.querySelector(`.trip-main__trip-controls h2:nth-of-type(1)`);
+render(menuInsertPoint, getMenuMarkup(), `afterEnd`);
+
+const filterInsertPoint = tripMain.querySelector(`.trip-main__trip-controls h2:nth-of-type(2)`);
+render(filterInsertPoint, getFilterMarkup(), `afterEnd`);
+
+const tripEventsSection = document.querySelector('.trip-events');
+render(tripEventsSection, getTripSortMarkup(), `beforeEnd`);
+
+render(tripEventsSection, getTripDaysMarkup(), 'beforeEnd');
+
+const dayEventsInsertPoint = tripEventsSection.querySelector(`.trip-events__list`);
+render(dayEventsInsertPoint, getEventEditFormMarkup(), 'beforeEnd');
+
+new Array(3).fill(``).forEach(() =>
+  render(dayEventsInsertPoint, getTripEventMarkup(), `beforeEnd`)
+);
