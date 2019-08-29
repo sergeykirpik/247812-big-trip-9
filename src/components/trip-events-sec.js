57a6ee6de95c21758f316510510323911b6c5ff7
Sergey Kirpik <sergeykirpik@gmail.com>
2019-08-29 22:31:08 +0300

    TripController

--- src/components/trip-events-sec.js
+++ src/components/trip-events-sec.js
@@ -1,42 +1,6 @@
-import {sortMethods} from "../data";
 import {AbstractComponent} from "./abstract-component";
-import {TripSort} from "./trip-sort";
-import {DayList} from "./day-list";
-import {NoPoints} from "./no-points";
-import {render, unrender} from "../utils";
 
 export class TripEventsSection extends AbstractComponent {
-  constructor(points) {
-    super();
-    this._routePoints = points;
-    this._sort = this.createOwnedComponent(new TripSort(sortMethods));
-    this._dayList = this.createOwnedComponent(new DayList(points));
-    this._noPoints = this.createOwnedComponent(new NoPoints());
-  }
-
-  update(points) {
-    unrender(this._dayList);
-    this._dayList = this.createOwnedComponent(new DayList(points));
-    render(this.element, this._dayList);
-  }
-
-  get sort() {
-    return this._sort;
-  }
-
-  get dayList() {
-    return this._dayList;
-  }
-
-  _afterElementCreated() {
-    if (this._routePoints.length > 0) {
-      render(this.element, this._sort);
-      render(this.element, this._dayList);
-    } else {
-      render(this.element, this._noPoints);
-    }
-  }
-
   get template() {
     return `
     <section class="trip-events">
--- src/controllers/trip-controller.js
+++ src/controllers/trip-controller.js
@@ -1,19 +1,37 @@
 import {TripEventsSection} from "../components/trip-events-sec";
-import {render} from "../utils";
+import {render, unrender} from "../utils";
 import {sortMethods} from "../data";
+import {TripSort} from "../components/trip-sort";
+import {DayList} from "../components/day-list";
+import {NoPoints} from "../components/no-points";
 
 export class TripController {
   constructor({container, points}) {
     this._container = container;
     this._points = points;
-    this._trip = new TripEventsSection(points);
+    this._tripEventsSection = new TripEventsSection();
+    this._sort = new TripSort(sortMethods);
+    this._dayList = new DayList(points);
+    this._noPoints = new NoPoints();
+  }
+
+  _renderTripEventsSection() {
+    if (this._points.length > 0) {
+      render(this._tripEventsSection.element, this._sort);
+      render(this._tripEventsSection.element, this._dayList);
+    } else {
+      render(this._tripEventsSection.element, this._noPoints);
+    }
+    render(this._container, this._tripEventsSection);
   }
 
   init() {
-    render(this._container, this._trip);
-    this._trip.sort.onSort((method) => {
+    this._renderTripEventsSection();
+    this._sort.onSort((method) => {
       const sorted = sortMethods[method](this._points);
-      this._trip.update(sorted);
+      unrender(this._dayList);
+      this._dayList = new DayList(sorted);
+      render(this._tripEventsSection.element, this._dayList);
     });
   }
 }
