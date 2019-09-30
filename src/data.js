0d220ffe08322974e91b2a49cd32dcbaa85ae829
Sergey Kirpik <sergeykirpik@gmail.com>
2019-09-30 23:05:32 +0300

    destructure operator replaced with Object.assign

--- src/data.js
+++ src/data.js
@@ -4,7 +4,6 @@ import {dataProvider} from "./data-provider";
 
 const MAX_DATE_INTERVAL = 10; // hours
 export const MAX_ROUTE_POINTS = 20;
-const RANDOM_PHOTO_COUNT = 5;
 
 export const getRandom = (n) => Math.floor(Math.random() * n);
 const getRandomBool = () => [true, false][getRandom(2)];
@@ -61,23 +60,6 @@ export const availableOffers = {
     price: 40,
   },
 };
-
-const lorem = [
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
-  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
-  `Fusce tristique felis at fermentum pharetra.`,
-  `Aliquam id orci ut lectus varius viverra.`,
-  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
-  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-  `Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat.`,
-  `Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`,
-];
-const getRandomDescription = () => shuffle(lorem.slice()).slice(0, getRandom(2) + 1).join(``);
-
-const getRandomPhoto = () => `http://picsum.photos/300/150?r=${Math.random()}`;
-
-const getRandomPhotos = () => new Array(RANDOM_PHOTO_COUNT).fill(``).map(getRandomPhoto);
 
 const getTripEvent = () => new PointModel({
   startTime: null,
